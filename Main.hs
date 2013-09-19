19d1be7f8bb3feab16e4f467e7fcafec15cde063
Fronx <fronx@soundcloud.com>
2013-09-19 20:51:38 +0200

    initial

---
+++ Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Simple
+
+main :: IO ()
+main = do
+  print $ eval (Add (Number 3)
+                    (Number 4))
+  print $ eval (Multiply (Add (Number 1)
+                              (Number 2))
+                         (Add (Number 3)
+                              (Multiply (Number 2)
+                                        (Number 2))))
+  print $ eval (LessThan (Number 10)
+                         (Add (Number 7)
+                              (Number 2)))
---
+++ Simple.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE
+      MultiParamTypeClasses,
+      FlexibleInstances,
+      FunctionalDependencies #-}
+
+module Simple where
+
+data IntExpr = Number   Int
+             | Add      IntExpr IntExpr
+             | Multiply IntExpr IntExpr
+
+data BoolExpr = Boolean  Bool
+              | LessThan IntExpr IntExpr
+
+class Expr a b | a -> b where
+  eval :: a -> b
+
+instance Expr IntExpr Int where
+  eval (Number x)     = x
+  eval (Add      x y) = eval(x) + eval(y)
+  eval (Multiply x y) = eval(x) * eval(y)
+
+instance Expr BoolExpr Bool where
+  eval (Boolean x)    = x
+  eval (LessThan a b) = eval(a) < eval(b)
