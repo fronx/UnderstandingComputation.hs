971a026e2b13df99fbb8d8cb10c463215c2a575f
Fronx <fronx@soundcloud.com>
2013-09-23 10:36:18 +0200

    pre-wire all the functions!

--- Simple.hs
+++ Simple.hs
@@ -12,19 +12,18 @@ data BoolExpr = Boolean  Bool
               | LessThan IntExpr IntExpr
 
 class Expr a b | a -> b where
-  eval   :: a -> a
   native :: a -> b
   box    :: b -> a
+  eval   :: a -> a
+  eval = box . native
 
 instance Expr IntExpr Int where
   native (Number x)     = x
   native (Add      x y) = native(x) + native(y)
   native (Multiply x y) = native(x) * native(y)
-  eval  = box . native
   box x = Number x
 
 instance Expr BoolExpr Bool where
   native (Boolean x)    = x
   native (LessThan a b) = native(a) < native(b)
-  eval  = box . native
   box x = Boolean x
